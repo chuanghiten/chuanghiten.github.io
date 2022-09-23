314a586c2b2f3023a6445709f7b7ad7fa3215de4
chuanghiten <thuphan12321@gmail.com>
2022-09-24 00:24:52 +0700

    Thứ sáu, 23 Tháng 9 năm 2022 18:32:47 +07 - H81M-S2PV

--- hisProject/weatherDigitalClock/javascript/script.js
+++ hisProject/weatherDigitalClock/javascript/script.js
@@ -75,7 +75,7 @@ async function positionData(position) {
 setInterval(function () {
   time = new Date();
   secondTime = time.getSeconds();
-  secondTime=0;
+//  secondTime=0;
   secondDeg = (secondTime * 360) / 60;
   minuteTime = time.getMinutes();
   minuteDeg = (minuteTime * 360) / 60;
