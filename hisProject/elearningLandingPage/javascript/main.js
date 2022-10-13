6adb8f86237af37b090208a9d52581900a59f671
chuanghiten <thuphan12321@gmail.com>
2022-10-13 20:26:38 +0700

    Thứ năm, 13 Tháng 10 năm 2022 19:41:27 +07 - H81M-S2PV

--- hisProject/elearningLandingPage/javascript/main.js
+++ hisProject/elearningLandingPage/javascript/main.js
@@ -5,3 +5,30 @@ window.onscroll=function(){
 		document.querySelector(".topBar").removeAttribute("scroll");
 	}
 }
+if(/duckBatman/.test(window.location.href)){
+	document.querySelector("body .mainContents .features .videos .mainVideo .videoPlayer").setAttribute("src","./mp4/Vua Trò Cheems 2.mp4");
+	document.querySelector("body .mainContents .features .videos .mainVideo .control").setAttribute("style","cursor:help");
+	document.querySelector("body .mainContents .features .videos .moreVideos .lecture2").setAttribute("style","cursor:help");
+	document.querySelector("body .mainContents .features .videos .moreVideos .lecture3").setAttribute("style","cursor:help");
+	document.querySelector("body .mainContents .features .videos .moreVideos .lecture4").setAttribute("style","cursor:help");
+	document.querySelector("body .mainContents .features .videos .mainVideo .control").onclick=function(){
+		document.querySelector("body .mainContents .features .videos .mainVideo .videoPlayer").setAttribute("controls","");
+		document.querySelector("body .mainContents .features .videos .mainVideo .videoPlayer").play();
+		document.querySelector("body .mainContents .features .videos .mainVideo .control").setAttribute("hide","");
+	}
+	document.querySelector("body .mainContents .features .videos .moreVideos .lecture2").onclick=function(){
+		document.querySelector("body .mainContents .features .videos .mainVideo .control").removeAttribute("hide");
+		document.querySelector("body .mainContents .features .videos .mainVideo .videoPlayer").removeAttribute("controls");
+		document.querySelector("body .mainContents .features .videos .mainVideo .videoPlayer").setAttribute("src","./mp4/Vua Trò Cheems.mp4");
+	}
+	document.querySelector("body .mainContents .features .videos .moreVideos .lecture3").onclick=function(){
+		document.querySelector("body .mainContents .features .videos .mainVideo .control").removeAttribute("hide");
+		document.querySelector("body .mainContents .features .videos .mainVideo .videoPlayer").removeAttribute("controls");
+		document.querySelector("body .mainContents .features .videos .mainVideo .videoPlayer").setAttribute("src","./mp4/Vua Trò Cheems 2.mp4");
+	}
+	document.querySelector("body .mainContents .features .videos .moreVideos .lecture4").onclick=function(){
+		document.querySelector("body .mainContents .features .videos .mainVideo .control").removeAttribute("hide");
+		document.querySelector("body .mainContents .features .videos .mainVideo .videoPlayer").removeAttribute("controls");
+		document.querySelector("body .mainContents .features .videos .mainVideo .videoPlayer").setAttribute("src","./mp4/Cách đối phó với các fan cuồng Rapper.mp4");
+	}
+}
