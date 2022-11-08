f7b9ad3341d9575e33602b54684fe29bfcf212c9
chuanghiten <thuphan12321@gmail.com>
2022-11-08 09:55:58 +0700

    Thứ ba, 08 Tháng 11 năm 2022 09:54:03 +07 - H81M-S2PV

--- hisProject/ies/js/main.js
+++ hisProject/ies/js/main.js
@@ -1,1 +1,6 @@
-window.document.querySelector('body').setAttribute('style',`--innerWidth:${window.innerWidth}px;--innerHeight:${window.innerHeight}px;`);
+var bodyElement = window.document.querySelector('body');
+function addScreenSizeToBodyElement () {
+	bodyElement.setAttribute('style',`--innerWidth:${window.innerWidth}px;--innerHeight:${window.innerHeight}px;`);
+}
+bodyElement.onresize = addScreenSizeToBodyElement;
+addScreenSizeToBodyElement();
