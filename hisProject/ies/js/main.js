80b1a6db58f5acf9cad43c1219d00e165cb98159
chuanghiten <thuphan12321@gmail.com>
2022-11-11 11:12:05 +0700

    Thứ năm, 10 Tháng 11 năm 2022 21:20:07 +07 - H81M-S2PV

--- hisProject/ies/js/main.js
+++ hisProject/ies/js/main.js
@@ -4,3 +4,54 @@ function addScreenSizeToBodyElement () {
 }
 addScreenSizeToBodyElement();
 bodyElement.onresize = addScreenSizeToBodyElement;
+window.document.querySelector("input#fileInput").onchange=function(event){
+	let uploadedFile = event.target.files[0];
+	//console.log(uploadedFile);
+	let readFile = new FileReader();
+	readFile.readAsText(uploadedFile);
+	readFile.onload = function(e) { 
+		let contents = e.target.result;
+		let json = JSON.parse(contents);
+		console.log(json);
+	};
+};
+window.document.querySelector('.dropFile').ondragover=window.document.querySelector('.dropFile').ondrop=window.document.querySelector('.dropFile').ondragover=function(event){
+	event.preventDefault();
+}
+window.document.querySelector('.dropFile').ondrop=function(event){
+	event.preventDefault();
+	/*
+	readFile.onload = function(e) { 
+		let contents = e.target.result;
+		let json = JSON.parse(contents);
+		console.log(json);
+	};
+	*/
+}
+/*
+$("#up").change(function(event){
+    var uploadedFile = event.target.files[0]; 
+    
+		if(uploadedFile.type !== "text/javascript" && uploadedFile.type !== "application/x-javascript") { 
+        alert("Wrong file type == " + uploadedFile.type); 
+        return false;
+    }
+    
+    if (uploadedFile) {
+        var readFile = new FileReader();
+        readFile.onload = function(e) { 
+            var contents = e.target.result;
+            var json = JSON.parse(contents);
+            alert_data(json);
+        };
+        readFile.readAsText(uploadedFile);
+    } else { 
+        console.log("Failed to load file");
+    }
+});
+
+function alert_data(json)
+{
+	alert('Name : ' + json.name + ', Family : '+ json.family)
+}
+*/
