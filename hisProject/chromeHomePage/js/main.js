b74c1fa5daaea79b15f720c60eeedb5367519b0f
chuanghiten <thuphan12321@gmail.com>
2022-12-31 22:27:00 +0700

    Thứ bảy, 31 Tháng 12 năm 2022 19:56:11 +07 - H81M-S2PV

--- hisProject/chromeHomePage/js/main.js
+++ hisProject/chromeHomePage/js/main.js
@@ -1,18 +1,40 @@
 var bodyElement = window.document.querySelector("body"),
   inputCommand = document.querySelector("#inputCommands"),
-  lableElement = document.querySelector("label");
+  labelElement = document.querySelector("label");
 function addScreenSizeToBodyElement() {
   bodyElement.setAttribute(
     "style",
     `--innerWidth:${window.innerWidth}px;--innerHeight:${window.innerHeight}px;`
   );
 }
+function addNewHistoryCommand(command) {
+  const newHistoryCommand = document.createElement("div");
+  const contentCommand = document.createTextNode(command);
+  newHistoryCommand.appendChild(contentCommand);
+  newHistoryCommand.setAttribute("class", "commandHistory");
+  labelElement.appendChild(newHistoryCommand);
+}
 addScreenSizeToBodyElement();
 bodyElement.onresize = addScreenSizeToBodyElement;
 inputCommand.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     event.preventDefault();
-    //    console.log(inputCommand.value);
-    lableElement.innerHTML = inputCommand.value;
+    //console.log(inputCommand.value);
+    //labelElement.innerHTML = inputCommand.value;
+    if (inputCommand.value.toLowerCase() == "clear") {
+      labelElement.innerHTML = "";
+    } else if (inputCommand.value.slice(0, 3).toLowerCase() == "url") {
+      if (
+        inputCommand.value.slice(4).toLowerCase() != "http://" ||
+        inputCommand.value.slice(4).toLowerCase() != "https://"
+      ) {
+        window.location = "http://" + inputCommand.value.slice(4);
+      } else {
+        window.location = inputCommand.value.slice(4);
+      }
+    } else {
+      addNewHistoryCommand(inputCommand.value);
+    }
+    inputCommand.value = "";
   }
 });
