7c69bb1de6b00886cc299c8dda086fcb00e316a1
chuanghiten <thuphan12321@gmail.com>
2022-12-31 19:56:26 +0700

    Thứ bảy, 31 Tháng 12 năm 2022 19:56:11 +07 - H81M-S2PV

--- hisProject/chromeHomePage/js/main.js
+++ hisProject/chromeHomePage/js/main.js
@@ -1,9 +1,16 @@
-var bodyElement = window.document.querySelector("body");
+var bodyElement = window.document.querySelector("body"),
+  inputCommand = document.querySelector("#inputCommands");
 function addScreenSizeToBodyElement() {
-    bodyElement.setAttribute(
-        "style",
-        `--innerWidth:${window.innerWidth}px;--innerHeight:${window.innerHeight}px;`
-    );
+  bodyElement.setAttribute(
+    "style",
+    `--innerWidth:${window.innerWidth}px;--innerHeight:${window.innerHeight}px;`
+  );
 }
 addScreenSizeToBodyElement();
 bodyElement.onresize = addScreenSizeToBodyElement;
+inputCommand.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    console.log(inputCommand.value);
+  }
+});
