cfd77be360e6b65e52ed4c81491970ee47b1b7e6
chuanghiten <thuphan12321@gmail.com>
2022-12-31 18:38:34 +0700

    hp

---
+++ hisProject/chromeHomePage/js/main.js
@@ -0,0 +1,9 @@
+var bodyElement = window.document.querySelector("body");
+function addScreenSizeToBodyElement() {
+    bodyElement.setAttribute(
+        "style",
+        `--innerWidth:${window.innerWidth}px;--innerHeight:${window.innerHeight}px;`
+    );
+}
+addScreenSizeToBodyElement();
+bodyElement.onresize = addScreenSizeToBodyElement;
