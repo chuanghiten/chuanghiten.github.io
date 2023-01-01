95018bc35d30f23a41ff72f159a41eaafa521697
chuanghiten <thuphan12321@gmail.com>
2023-01-01 12:52:30 +0700

    Sun Jan  1 08:18:22 +07 2023 - Redmi 9Nya

--- hisProject/chromeHomePage/js/main.js
+++ hisProject/chromeHomePage/js/main.js
@@ -10,13 +10,14 @@ function addScreenSizeToBodyElement() {
 function addNewHistoryCommand(command) {
   const newHistoryCommand = document.createElement("div");
   const contentCommand = document.createTextNode(command);
-  newHistoryCommand.appendChild(contentCommand);
+  newHistoryCommand.appendChild(contentCommand);		
+	
   newHistoryCommand.setAttribute("class", "commandHistory");
   labelElement.appendChild(newHistoryCommand);
 }
 addScreenSizeToBodyElement();
 bodyElement.onresize = addScreenSizeToBodyElement;
-inputCommand.addEventListener("input", (event) => displayCommandInput.textContent = event.target.value)
+inputCommand.addEventListener("input", (event) => {displayCommandInput.innerHTML = event.target.value})
 inputCommand.addEventListener("keypress", (event) => {
   if (event.key === "Enter") {
     event.preventDefault();
@@ -33,8 +34,26 @@ inputCommand.addEventListener("keypress", (event) => {
       } else {
         window.location = inputCommand.value.slice(4);
       }
+    }else if (inputCommand.value.toLowerCase()=="help"){
+    	addNewHistoryCommand("Đây không phải là một môi trường linux thực sự mà chỉ là một trình giả lập lấy ý tưởng từ linux, vậy nên chương trình này chỉ có thể chạy những lệnh được hỗ trợ. Danh sách những lệnh được hỗ trợ được liệt kê dưới đây:");
+    	addNewHistoryCommand("- url   : Được dùng để mở một đường link được chỉ định")
+    	addNewHistoryCommand("- clear: Được dùng để xóa toàn bộ nội dung trên màn hình")
+    	addNewHistoryCommand("Mọi lệnh ở danh sách trên đều có thể sử dụng kèm với đối số \"-h\" hoặc \"--help\" để có thể biết thêm chi tiết cách dùng lệnh")
     } else {
-      addNewHistoryCommand(inputCommand.value);
+    		if (inputCommand.value.slice(0,1).toLowerCase()=="c"){
+    			addNewHistoryCommand("Command \""+inputCommand.value+"\" not found, did you mean:");
+    			addNewHistoryCommand("- clear");
+    			addNewHistoryCommand("or use \"help\" command.");
+    			//addNewHistoryCommand("- clear")
+    		} else if (inputCommand.value.slice(0,1).toLowerCase()=="u"){
+    			addNewHistoryCommand("Command \""+inputCommand.value+"\" not found, did you mean:");
+    			addNewHistoryCommand("- url");
+    			addNewHistoryCommand("or use \"help\" command.");
+    			//addNewHistoryCommand("- clear")
+    		} else{
+    			addNewHistoryCommand("Command \""+inputCommand.value+"\" not found! Use \"help\" command?");
+    		}
+      //addNewHistoryCommand(inputCommand.value);
     }
     labelElement.scrollTop = labelElement.scrollHeight;
     inputCommand.value = "";
