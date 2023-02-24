be914acd2022aa4aaadd54c14e5d18c47aa13d44
chuanghiten <thuphan12321@gmail.com>
2023-02-24 20:24:58 +0700

    Fri Feb 24 20:09:05 +07 2023 - Redmi 9Nya

--- hisProject/dateAndWeather/javascript/main.js
+++ hisProject/dateAndWeather/javascript/main.js
@@ -1,18 +1,11 @@
-var bodyElement = window.document.querySelector("body"),fullScreenButton = window.document.querySelector(".fullScreenButton");
+var bodyElement = window.document.querySelector("body"),goFS = document.getElementById("goFS");
 function addScreenSizeToBodyElement() {
   bodyElement.setAttribute(
     "style",
     `--innerWidth:${window.innerWidth}px;--innerHeight:${window.innerHeight}px;`
   );
 }
-function fullScreen(){
-	if (document.documentElement.requestFullscreen) {
-		document.documentElement.requestFullscreen
-	}
-}
-function consoleTest(){
-	console.log(true);
-}
+bodyElement.addEventListener("click", () => {if (document.documentElement.requestFullscreen) {document.documentElement.requestFullscreen()}}, false);
 addScreenSizeToBodyElement();
 bodyElement.onresize = addScreenSizeToBodyElement;
-window.document.querySelector(".background").onclick = fullScreen;
+
