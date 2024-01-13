0eb1e381a493d5eb85bc935aabbc9e7fcc33b333
chuanghiten <thuphan12321@gmail.com>
2024-01-13 23:48:51 +0700

    update

--- hisProject/timeAndWeather/javascript/main.js
+++ hisProject/timeAndWeather/javascript/main.js
@@ -1,15 +1,17 @@
-let mainDom = window.document.querySelector("html body .main");
+let mainDom = window.document.querySelector("html body .main"), mainDomWidth;
+let htmlDom = window.document.querySelector("html");
 
 function resize(width, height) {
 	if ((width / height) >= 0.480410447761194) {
-		mainDom.setAttribute("style", `width: ${height * 0.480410447761194}px; height: ${height}px`);
-		// mainDom.style.height = height;
-		// mainDom.style.width = height * 0.480410447761194;
+		mainDomWidth = height * 0.480410447761194;
 	} else {
-		mainDom.setAttribute("style", `height: ${width * 2.0815533980582526}px; width: ${width}px`);
-		// mainDom.style.width = width;
-		// mainDom.style.height = width * 2.0815533980582526;
+		mainDomWidth = width;
 	}
+	mainDom.style.width = `${mainDomWidth}px`;
+	mainDom.style.height = `${height}px`;
+	// console.log(6.25 * mainDomWidth / 515);
+	htmlDom.style.fontSize = `${6.25 * mainDomWidth / 515}%`;
+
 }
 
 function main() {
