809ae4737e423199546998dccb81a2569df67c29
chuanghiten <thuphan12321@gmail.com>
2024-01-13 20:08:24 +0700

    Update

--- hisProject/timeAndWeather/javascript/main.js
+++ hisProject/timeAndWeather/javascript/main.js
@@ -1,13 +1,20 @@
-let body = window.document.querySelector("body .fullScreen");
+let mainDom = window.document.querySelector("html body .main");
 
 function resize(width, height) {
-	console.log(`width: ${width}, height: ${height}`);
-	body.innerHTML = `width: ${width}, height: ${height}`;
+	if ((width / height) >= 0.480410447761194) {
+		mainDom.setAttribute("style", `width: ${height * 0.480410447761194}px; height: ${height}px`);
+		// mainDom.style.height = height;
+		// mainDom.style.width = height * 0.480410447761194;
+	} else {
+		mainDom.setAttribute("style", `height: ${width * 2.0815533980582526}px; width: ${width}px`);
+		// mainDom.style.width = width;
+		// mainDom.style.height = width * 2.0815533980582526;
+	}
 }
 
 function main() {
+	resize(window.innerWidth, window.innerHeight);
 	window.addEventListener("resize", () => { resize(window.innerWidth, window.innerHeight) });
-	body.addEventListener("click", () => { window.document.querySelector("html").requestFullscreen() });
 }
 
 window.addEventListener("load", () => { main() });
