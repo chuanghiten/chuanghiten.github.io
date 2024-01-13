d06978b1b318e18f22e8f8c4c144c0d8453fcbb9
chuanghiten <thuphan12321@gmail.com>
2024-01-13 15:20:01 +0700

    Clear

--- hisProject/figmaStartupLandingPageDark/javascript/main.js
+++
@@ -1,7 +0,0 @@
-window.onscroll=function(){
-	if (document.documentElement.scrollTop > 50){
-		document.querySelector("body .topBar").setAttribute("scroll","");
-	} else {
-		document.querySelector("body .topBar").removeAttribute("scroll");
-	}
-}
