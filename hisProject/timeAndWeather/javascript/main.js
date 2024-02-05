8f6aad81cfd0648ae0d46c83e2ddfb9fb38d10ae
chuanghiten <thuphan12321@gmail.com>
2024-02-05 10:16:17 +0700

    Changes to be committed: 	modified:   .sass-cache/165368b0779b58bc557b8f0abe9715f35547d47f/main.sassc 	modified:   hisProject/timeAndWeather/css/main.css 	modified:   hisProject/timeAndWeather/css/main.css.map 	modified:   hisProject/timeAndWeather/index.html 	new file:   hisProject/timeAndWeather/javascript/anime.js 	modified:   hisProject/timeAndWeather/javascript/main.js 	modified:   hisProject/timeAndWeather/sass/main.sass

--- hisProject/timeAndWeather/javascript/main.js
+++ hisProject/timeAndWeather/javascript/main.js
@@ -106,7 +106,10 @@ let mainDom = window.document.querySelector("html body .main"),
   updateBy = window.document.querySelector(
     "html body .main .contents .weather .updateBy"
   ),
-  locationKey = false;
+  locationKey = false,
+  cloud1Anime,
+  cloud2Anime,
+  cloud3Anime;
 
 function add0(number) {
   if (number < 10) return `0${number}`;
@@ -391,7 +394,91 @@ async function callNetlify(lat, lon, locationKey, ip) {
 function updateWeather(name, value) {
   switch (name) {
     case "wind":
-      clouds.setAttribute("style", `--cloudsDuration: ${5 / (value / 114)}s`);
+      if (!cloud1Anime) {
+        cloud1Anime = anime({
+          targets: clouds.children[0],
+          translateX: ["-449rem", "886rem"],
+          easing: "linear",
+          duration: 3000 / (value / 144),
+          delay: 1500,
+          loop: true,
+        });
+      } else {
+        let cloud1CurrentTransform = Number(
+          cloud1Anime.animations[0].currentValue.replace("rem", "")
+        );
+        cloud1Anime = anime({
+          targets: clouds.children[0],
+          translateX: ["-449rem", "886rem"],
+          // translateX: "886rem",
+          easing: "linear",
+          duration: 3000 / (value / 144),
+          delay: 1500,
+          loop: true,
+          autoplay: false,
+        });
+        cloud1Anime.seek(
+          cloud1Anime.duration * ((cloud1CurrentTransform + 449) / (886 + 449))
+        );
+        cloud1Anime.play();
+      }
+      if (!cloud2Anime) {
+        cloud2Anime = anime({
+          targets: clouds.children[1],
+          translateX: ["-794rem", "541rem"],
+          easing: "linear",
+          duration: 1000 / (value / 144),
+          delay: 1500,
+          loop: true,
+        });
+      } else {
+        let cloud2CurrentTransform = Number(
+          cloud2Anime.animations[0].currentValue.replace("rem", "")
+        );
+        cloud2Anime = anime({
+          targets: clouds.children[1],
+          translateX: ["-794rem", "541rem"],
+          // translateX: "541rem",
+          easing: "linear",
+          duration: 1000 / (value / 144),
+          delay: 1500,
+          loop: true,
+          autoplay: false,
+        });
+        cloud2Anime.seek(
+          cloud2Anime.duration * ((cloud2CurrentTransform + 794) / (541 + 794))
+        );
+        cloud2Anime.play();
+      }
+      if (!cloud3Anime) {
+        cloud3Anime = anime({
+          targets: clouds.children[2],
+          translateX: ["-1107rem", "228rem"],
+          // translateX: "228rem",
+          easing: "linear",
+          duration: 5000 / (value / 144),
+          delay: 1500,
+          loop: true,
+        });
+      } else {
+        let cloud3CurrentTransform = Number(
+          cloud3Anime.animations[0].currentValue.replace("rem", "")
+        );
+        cloud3Anime = anime({
+          targets: clouds.children[2],
+          translateX: ["-1107rem", "228rem"],
+          // translateX: "228rem",
+          easing: "linear",
+          duration: 5000 / (value / 144),
+          delay: 1500,
+          loop: true,
+          autoplay: false,
+        });
+        cloud3Anime.seek(
+          cloud3Anime.duration * ((cloud3CurrentTransform + 1107) / (228 + 1107))
+        );
+        cloud3Anime.play();
+      }
       break;
     case "trangThai":
       trangThai.innerHTML = `${value[0].toUpperCase()}${value.slice(1)}`;
@@ -721,7 +808,7 @@ function updateDoThi(value) {
 //   ],
 // });
 
-function updateTime(name, value) {
+function pushTime(name, value) {
   switch (name) {
     case "progressThangAm":
       arrowThangAm.setAttribute("style", `--left: ${value * 100}%`);
@@ -958,33 +1045,35 @@ function main() {
       });
     });
   else alert("Geolocation is not supported by this browser.");
-  setInterval(() => {
+  // setInterval(() => , 100);
+
+  function updateTime() {
     time = new Date();
     newSeconds = time.getSeconds();
-    updateTime("second", newSeconds * 1000 + time.getMilliseconds());
+    pushTime("second", newSeconds * 1000 + time.getMilliseconds());
     if (oldSeconds != newSeconds) {
       oldSeconds = newSeconds;
       newMinutes = time.getMinutes();
-      updateTime("minutesArrow", 6 * (newMinutes + newSeconds / 60));
-      updateTime("progressDay", newHours * 3600 + newMinutes * 60 + newSeconds);
+      pushTime("minutesArrow", 6 * (newMinutes + newSeconds / 60));
+      pushTime("progressDay", newHours * 3600 + newMinutes * 60 + newSeconds);
       if (oldMinutes != newMinutes) {
         oldMinutes = newMinutes;
         newHours = time.getHours();
-        updateTime("minutes", newMinutes);
-        updateTime("hoursArrow", 30 * (newHours + newMinutes / 60));
+        pushTime("minutes", newMinutes);
+        pushTime("hoursArrow", 30 * (newHours + newMinutes / 60));
         if (
           (newMonth <= 7 && newMonth % 2 != 0) ||
           (newMonth >= 8 && newMonth % 2 == 0)
         ) {
           thangDuong.style.textDecoration = "underline";
-          updateTime(
+          pushTime(
             "progressThangDuong",
             (newDate * 86400 + newHours * 3600 + newMinutes * 60) / 2764740
           );
         } else {
           thangDuong.style.textDecoration = "none";
           if (newMonth != 2)
-            updateTime(
+            pushTime(
               "progressThangDuong",
               (newDate * 86400 + newHours * 3600 + newMinutes * 60) / 2678340
             );
@@ -994,13 +1083,13 @@ function main() {
               (newYear.toString().slice(-2) != "00" && newYear % 4 == 0)
             ) {
               namDuong.style.textDecoration = "underline";
-              updateTime(
+              pushTime(
                 "progressThangDuong",
                 (newDate * 86400 + newHours * 3600 + newMinutes * 60) / 2591940
               );
             } else {
               namDuong.style.textDecoration = "none";
-              updateTime(
+              pushTime(
                 "progressThangDuong",
                 (newDate * 86400 + newHours * 3600 + newMinutes * 60) / 2505540
               );
@@ -1009,7 +1098,7 @@ function main() {
         }
         if (soNgayAmTrongThang == 30) {
           thangAm.style.textDecoration = "underline";
-          updateTime(
+          pushTime(
             "progressThangAm",
             (getLunar(newDate, newMonth, newYear, 7)[0] * 86400 +
               newHours * 3600 +
@@ -1018,7 +1107,7 @@ function main() {
           );
         } else {
           thangAm.style.textDecoration = "none";
-          updateTime(
+          pushTime(
             "progressThangAm",
             (getLunar(newDate, newMonth, newYear, 7) * 86400 +
               newHours * 3600 +
@@ -1029,13 +1118,13 @@ function main() {
         if (oldHours != newHours) {
           oldHours = newHours;
           newDate = time.getDate();
-          updateTime("hour", newHours);
+          pushTime("hour", newHours);
           if (oldDate != newDate) {
             oldDate = newDate;
             newMonth = time.getMonth() + 1;
-            updateTime("ngayAm", getLunar(newDate, newMonth, newYear, 7)[0]);
-            updateTime("ngayDuong", newDate);
-            updateTime("thu", time.getDay() + 1);
+            pushTime("ngayAm", getLunar(newDate, newMonth, newYear, 7)[0]);
+            pushTime("ngayDuong", newDate);
+            pushTime("thu", time.getDay() + 1);
             soNgayAmTrongThang =
               getNewMoonDay(
                 INT(
@@ -1054,8 +1143,8 @@ function main() {
             if (oldMonth != newMonth) {
               oldMonth = newMonth;
               newYear = time.getFullYear();
-              updateTime("thangAm", getLunar(newDate, newMonth, newYear, 7)[1]);
-              updateTime("thangDuong", newMonth);
+              pushTime("thangAm", getLunar(newDate, newMonth, newYear, 7)[1]);
+              pushTime("thangDuong", newMonth);
               if (oldYear != newYear) {
                 if (
                   (newYear.toString().slice(-2) == "00" &&
@@ -1067,14 +1156,11 @@ function main() {
                 oldYear = newYear;
                 if (newYear == getLunar(newDate, newMonth, newYear, 7)[2]) {
                   namAm.style.display = "none";
-                  updateTime("namDuong", newYear);
+                  pushTime("namDuong", newYear);
                 } else {
                   namAm.style.display = "inline";
-                  updateTime("namDuong", newYear);
-                  updateTime(
-                    "namAm",
-                    getLunar(newDate, newMonth, newYear, 7)[2]
-                  );
+                  pushTime("namDuong", newYear);
+                  pushTime("namAm", getLunar(newDate, newMonth, newYear, 7)[2]);
                 }
               }
             }
@@ -1089,7 +1175,9 @@ function main() {
         }
       }
     }
-  }, 100);
+    window.requestAnimationFrame(updateTime);
+  }
+  updateTime();
 }
 
 window.addEventListener("load", () => {
