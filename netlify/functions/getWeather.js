2f3c2c1e47a49513fda84c3a40cf757f6b1cfc8e
chuanghiten <thuphan12321@gmail.com>
2024-02-07 18:04:17 +0700

    modified:   netlify/functions/getWeather.js

--- netlify/functions/getWeather.js
+++ netlify/functions/getWeather.js
@@ -4,8 +4,8 @@ let HEADERS = {
   "Access-Control-Allow-Headers":
     "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
   "Content-Type": "application/json", //optional
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
-  "Access-Control-Max-Age": "8640",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, POST, DELETE, OPTIONS",
+  "Access-Control-Max-Age": "86400",
 };
 
 //This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
