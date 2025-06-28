c429c4378bd246d2ffda2b2deabf8b78a2eedbe7
ChuaNghiTen <thuphan12321@gmail.com>
2025-06-28 07:21:59 +0700

    Update vi_lunar_calendar.js

--- netlify/functions/vi_lunar_calendar.js
+++ netlify/functions/vi_lunar_calendar.js
@@ -1,6 +1,7 @@
 const { stream } = require("@netlify/functions");
 
-exports.handler = stream(async () => {
+exports = {
+  handler: stream(async () => {
   const encoder = new TextEncoder();
   const formatter = new Intl.DateTimeFormat("en", { timeStyle: "medium" });
   const body = new ReadableStream({
@@ -28,8 +29,8 @@ exports.handler = stream(async () => {
     statusCode: 200,
     body
   };
-});
-
-exports.config = {
-  path: "vi_lunar_calendar"
+}),
+  config: {
+    path: "vi_lunar_calendar"
+  }
 }
