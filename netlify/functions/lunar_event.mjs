df7ff4a69249b0b09718aad5243c7d67b9443286
ChuaNghiTen <thuphan12321@gmail.com>
2025-06-28 20:37:54 +0700

    Update lunar_event.mjs

--- netlify/functions/lunar_event.mjs
+++ netlify/functions/lunar_event.mjs
@@ -4,30 +4,30 @@ export default stream(async (req, contex) => {
   console.log(req, contex);
   const encoder = new TextEncoder();
   const formatter = new Intl.DateTimeFormat("en", { timeStyle: "medium" });
-  const body = new ReadableStream({
-    start(controller) {
-      controller.enqueue(encoder.encode("<html><body><ol>"));
-      let i = 0;
-      const timer = setInterval(() => {
-        controller.enqueue(
-          encoder.encode(
-            `<li>Hello at ${formatter.format(new Date())}</li>\n\n`
-          )
-        );
-        if (i++ >= 5) {
-          controller.enqueue(encoder.encode("</ol></body></html>"));
-          controller.close();
-          clearInterval(timer);
-        }
-      }, 100);
-    }
-  });
+  // const body = ;
   return {
     headers: {
       "content-type": "text/html"
     },
     statusCode: 200,
-    body: body
+    body: new ReadableStream({
+      start(controller) {
+        controller.enqueue(encoder.encode("<html><body><ol>"));
+        let i = 0;
+        const timer = setInterval(() => {
+          controller.enqueue(
+            encoder.encode(
+              `<li>Hello at ${formatter.format(new Date())}</li>\n\n`
+            )
+          );
+          if (i++ >= 5) {
+            controller.enqueue(encoder.encode("</ol></body></html>"));
+            controller.close();
+            clearInterval(timer);
+          }
+        }, 100);
+      }
+    })
   };
 });
 
