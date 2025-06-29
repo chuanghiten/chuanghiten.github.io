678e3ef2a65ecc08021610dd40e727f306d17450
ChuaNghiTen <thuphan12321@gmail.com>
2025-06-29 11:44:54 +0700

    Update lunar_event.mjs

--- netlify/functions/lunar_event.mjs
+++ netlify/functions/lunar_event.mjs
@@ -1,21 +1,28 @@
-import { stream } from '@netlify/functions';
+export default async () => {
+  const encoder = new TextEncoder();
+  const formatter = new Intl.DateTimeFormat("en", { timeStyle: "medium" });
+  const body = new ReadableStream({
+    start(controller) {
+      controller.enqueue(encoder.encode("<html><body><ol>"));
+      let i = 0;
+      const timer = setInterval(() => {
+        controller.enqueue(
+          encoder.encode(
+            `<li>Hello at ${formatter.format(new Date())}</li>\n\n`
+          )
+        );
+        if (i++ >= 5) {
+          controller.enqueue(encoder.encode("</ol></body></html>"));
+          controller.close();
+          clearInterval(timer);
+        }
+      }, 1000);
+    }
+  });
 
-export default stream(async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/plain',
-    },
-    body: new ReadableStream({
-      start(controller) {
-        controller.enqueue(new TextEncoder().encode('Hello, '));
-        controller.enqueue(new TextEncoder().encode('Streaming World!'));
-        controller.close();
-      },
-    }),
-  };
-});
+  return new Response(body);
+};
 
 export const config = {
-  path: "/vi_lunar_calendar"
-};
+    path: "/vi_lunar_calendar"
+}
