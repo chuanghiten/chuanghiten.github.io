21b58b4a6fabb7cbb25b197e192d541c379c29b7
ChuaNghiTen <thuphan12321@gmail.com>
2025-07-07 11:47:42 +0700

    Update vi_lunar_calendar.mjs

--- netlify/functions/vi_lunar_calendar.mjs
+++ netlify/functions/vi_lunar_calendar.mjs
@@ -363,7 +363,7 @@ export default async (req, context) => {
 
         const chi_of_day = (jdFromDate(Number(cr_date), Number(cr_month), cr_year) + 1) % 12;
 
-        controller.enqueue(encoder.encode(` năm ${can[(lunar[2] + 6) % 10]} ${chi[(lunar[2] + 8) % 12]} ${lunar[2]} (Âm lịch)\\n\\n\n\tNgày ${can[(jdFromDate(Number(cr_date), Number(cr_month), cr_year) + 9) % 10]} ${chi[chi_of_day]} (\n\t${nhat_tinh[list_ngay_hd[lunar[1] % 6][chi_of_day]]})\\n\n\tTháng `));
+        controller.enqueue(encoder.encode(` năm ${can[(lunar[2] + 6) % 10]} ${chi[(lunar[2] + 8) % 12]} ${lunar[2]} (Âm lịch)\\n\\n\n\tNgày ${can[(jdFromDate(Number(cr_date), Number(cr_month), cr_year) + 9) % 10]} ${chi[chi_of_day]} - \n\t${nhat_tinh[list_ngay_hd[lunar[1] % 6][chi_of_day]]}\\n\n\tTháng `));
 
         if (Number(lunar[1]) === 1) controller.enqueue(encoder.encode('Giêng '));
         else if (Number(lunar[1]) === 12) controller.enqueue(encoder.encode('Chạp '));
